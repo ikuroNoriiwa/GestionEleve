d0fc5ced736ee152c9386715522a0da88a96bde8
Mathieu Noyelle <37225914+ikuroNoriiwa@users.noreply.github.com>
2020-02-23 16:09:36 +0100

    [ADD-COMMENT] commentaires APP_creationEleve

--- APP/APP_creationEleve.c
+++ APP/APP_creationEleve.c
@@ -11,6 +11,17 @@
  #include "../defStruct.h"
  #include "../DATA/DATA_save.h"
 
+/**
+ * Crée et renvoie un struct Eleve
+ *
+ * @param nom : char* --> nom de l'élève
+ * @param prenom : char* --> prenom de l'élève
+ * @param promotion : char* --> promotion de l'élève
+ * @param id : int --> 0 si nouvelle création, > 0 si récupéré de la base
+ * @return newEleve: Struct Eleve* contenant les paramètres
+ *
+ * @author mathieu
+ */
 Eleve* createEleve(char* nom, char* prenom, char *promotion, int id ){
     Eleve* newEleve = (Eleve*)malloc(sizeof(Eleve));
     newEleve->nom = (char*)malloc(sizeof(char*)*sizeof(nom));
@@ -29,27 +40,73 @@ void saveEleves(Eleve* eleve){
     DATA_save__saveEleve(eleve);
 }
 
+/**
+ *  renvoie un tableau struct Eleve
+ *
+ * @return newEleve: Struct Eleve** contenant tous les Eleves en base
+ *
+ * @author mathieu
+ */
 Eleve** APP_creationEleve__getAllEleves(){
+
     return DATA_save__getEleve();
 }
+
+/**
+ *  renvoie le nombre d'Eleve
+ *
+ * @return nbEleve: int
+ *
+ * @author mathieu
+ */
  int APP_creationEleve__getNbEleve(){
     return DATA_save__retNbValTable_eleve();
  }
 
+/**
+ *  supprime un élève
+ *
+ * @param idEleve: int id de l'élève en base
+ *
+ * @author mathieu
+ */
 void APP_creationEleve__supprimerEleve(int idEleve){
     DATA_save__supprimerEleve(idEleve);
 }
 
+/**
+ *  renvoie un struct Eleve
+ *
+ * @param idEleve : int id de l'élève en base
+ * @return newEleve: Struct Eleve* contenant un Eleve en base
+ *
+ * @author mathieu
+ */
 Eleve* APP_creationEleve__getOneEleve(int idEleve){
     Eleve* tmpEleve = DATA_save__getOneEleve(idEleve);
     return tmpEleve;
 }
 
+/**
+ *  modifie un élève
+ *
+ * @param idEleve: int id de l'élève en base
+ *
+ * @author mathieu
+ */
 void APP_creationEleve__modifierEleve(Eleve* tmpEleve){
     DATA_save__modifierEleve(tmpEleve);
 
 }
 
+/**
+ *  renvoie un struct Eleve et ses notes
+ *
+ * @param idEleve : int id de l'élève en base
+ * @return newEleve: Struct Eleve* contenant un Eleve en base
+ *
+ * @author mathieu
+ */
 Eleve* APP_creationEleve__getOneElevePlusNotes(int idEleve){
     Eleve* tmpEleve = DATA_save__getOneEleve(idEleve);
     tmpEleve->listesNotes = DATA_save__getNotes(idEleve);
